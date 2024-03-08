b720b86ab8ad7367c2fca8986afc07a4bcc0ced7
kholoud001 <kholoud.sanak@gmail.com>
2024-03-08 14:33:06 +0100

    ticket blade

--- resources/views/event/ticket.blade.php
+++ resources/views/event/ticket.blade.php
@@ -1,60 +1,71 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
-        .num {
-            font-size: 60px;
-            text-align: center;
-            color: #111;
+        .event-description {
+            font-family: 'Inknut Antiqua', serif;
         }
 
-        .day {
-            text-align: center;
-            font-size: 25px;
+        .num {
+            font-family: 'Ravi Prakash', cursive;
+        }
+
+        .day,
+        .event {
+            font-family: 'Lora', serif;
+        }
+
+        .sce p,
+        .loc p {
+            font-family: 'Indie Flower', cursive;
+        }
+
+        .tickets,
+        .booked,
+        .cancel {
+            font-family: 'Cabin', sans-serif;
         }
     </style>
 </head>
-<body>
 
+<body class="bg-gray-300">
 
-<div class="container mx-auto max-w-7xl">
-    <div class="item bg-white p-0 sm:p-20 overflow-hidden my-10 border border-gray-300">
-        <div class="item-right float-left p-20">
-            <h2 class="num">{{ $ticket->event->deadline->format('d') }} {{ $ticket->event->deadline->format('M') }}</h2>
-            <p class="day">{{ $ticket->event->deadline->format('M') }}</p>
-            <span class="up-border"></span>
-            <span class="down-border"></span>
+<div class="container mx-auto">
+    <div class="item bg-white rounded overflow-hidden shadow-lg my-10 mx-auto md:w-1/2">
+        <div class="item-right p-10">
+            <h2 class="num text-6xl">{{ $ticket->event->deadline->format('d') }}</h2>
+            <p class="day text-2xl">{{ $ticket->event->deadline->format('M') }}</p>
+            <span class="up-border bg-gray-400 rounded-full p-3 absolute top-0 right-0"></span>
+            <span class="down-border bg-gray-400 rounded-full p-3 absolute bottom-0 right-0"></span>
         </div>
 
-        <div class="item-left w-4/5 sm:w-3/4 p-0 sm:p-34 border-l-3 border-dotted border-gray-300">
-            <p class="event">{{ $ticket->event->titre }}</p>
-            <h2 class="title">{{ $ticket->event->description }}</h2>
+        <div class="item-left border-l-4 border-dotted border-gray-900 p-8">
+            <p class="event text-2xl">{{ $ticket->event->titre }}</p>
+            <h2 class="title text-4xl">{{ $ticket->event->description }}</h2>
 
-            <div class="sce mt-5">
-                <div class="icon float-left mr-3">
+            <div class="sce flex items-center mt-5">
+                <div class="icon">
                     <i class="fa fa-table"></i>
                 </div>
                 <p>{{ $ticket->event->deadline->format('l jS Y') }} <br> {{ $ticket->created_at->format('H:i A') }}</p>
             </div>
-            <div class="clearfix"></div>
-            <div class="loc mt-5">
-                <div class="icon float-left mr-3">
+            <div class="loc flex items-center">
+                <div class="icon">
                     <i class="fa fa-map-marker"></i>
                 </div>
                 <p>{{ $ticket->event->lieu->ville }}</p>
             </div>
-            <div class="clearfix"></div>
-            <button class="tickets bg-gray-700 text-white px-14 py-6 float-right mt-10">Tickets</button>
+            <button class="tickets bg-gray-700 text-white px-6 py-2 mt-5">Tickets</button>
         </div>
     </div>
 </div>
 
-
 </body>
+
 </html>
