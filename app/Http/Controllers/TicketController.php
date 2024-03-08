6d5e699a365dc174d9a84e02a04fbcc8e27e753e
kholoud001 <kholoud.sanak@gmail.com>
2024-03-08 14:32:00 +0100

    ticket controller update

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ticket;
+use Illuminate\Support\Facades\Storage;
 use PDF;
 
 //use Barryvdh\DomPDF\Facade\Pdf;
@@ -13,7 +14,14 @@ class TicketController extends Controller
 
     public function download(Ticket $ticket)
     {
+        // Generate PDF
         $pdf = PDF::loadView('event.ticket', compact('ticket'));
+
+        // Store PDF in storage
+        $pdfPath = 'pdfs/ticket_' . $ticket->id . '.pdf'; // Adjust the path as needed
+        Storage::put($pdfPath, $pdf->output());
+
+        // Return download response
         return $pdf->download('ticket.pdf');
     }
 
