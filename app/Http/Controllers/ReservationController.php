5caa2e3e0a7bf04cec0967bfade9ce7ed970955c
kholoud001 <kholoud.sanak@gmail.com>
2024-03-09 10:18:45 +0100

    rseservation controller update

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -65,40 +65,40 @@ class ReservationController extends Controller
 
         return redirect()->route('event.single', $reservation->event_id);
     }
-    public function store1(Request $request)
-    {
-        // Validate the request data
-        $validatedData = $request->validate([
-            'email' => 'required|email',
-        ]);
-
-        // Find the event place and decrement the nombre_place
-        $eventPlace = Event::find($request->event_id);
-        if ($eventPlace) {
-            $eventPlace->decrement('nombre_place');
-        }
-
-        $user_id = Auth::id();
-
-        // Create a reservation
-        $reservation = Reservation::create([
-            'user_id' => $user_id,
-            'event_id' => $request->event_id,
-            'email' => $validatedData['email'],
-        ]);
-
-        // Create a ticket and associate it with the reservation
-        $ticket = new Ticket();
-        $ticket->user_id=$user_id;
-        $ticket->event_id = $request->event_id;
-        $ticket->ticket_number = rand(10000000, 99999999);
-        $reservation->ticket()->save($ticket);
-
-
-//        return redirect()->route('ticket.show', $ticket->id);
-        return redirect()->route('ticket.download', $ticket->id);
-
-    }
+//    public function store1(Request $request)
+//    {
+//        // Validate the request data
+//        $validatedData = $request->validate([
+//            'email' => 'required|email',
+//        ]);
+//
+//        // Find the event place and decrement the nombre_place
+//        $eventPlace = Event::find($request->event_id);
+//        if ($eventPlace) {
+//            $eventPlace->decrement('nombre_place');
+//        }
+//
+//        $user_id = Auth::id();
+//
+//        // Create a reservation
+//        $reservation = Reservation::create([
+//            'user_id' => $user_id,
+//            'event_id' => $request->event_id,
+//            'email' => $validatedData['email'],
+//        ]);
+//
+//        // Create a ticket and associate it with the reservation
+//        $ticket = new Ticket();
+//        $ticket->user_id=$user_id;
+//        $ticket->event_id = $request->event_id;
+//        $ticket->ticket_number = rand(10000000, 99999999);
+//        $reservation->ticket()->save($ticket);
+//
+//
+////        return redirect()->route('ticket.show', $ticket->id);
+//        return redirect()->route('ticket.download', $ticket->id);
+//
+//    }
 
     /////////////////////////////////          Organizer             ///////////////////////////////////////////
     ///
@@ -124,7 +124,6 @@ class ReservationController extends Controller
         $reservation->status = 1;
         $reservation->save();
 
-        // Send email notification to user
         Mail::to($reservation->email)->send(new ReservationApproved($reservation));
 
         return redirect()->back()->with('success', 'Reservation has been approved.');
@@ -133,9 +132,8 @@ class ReservationController extends Controller
     public function decline($id)
     {
         $reservation = Reservation::findOrFail($id);
-        $reservation->delete();
 
-        // Send email notification to user
+        $reservation->delete();
         Mail::to($reservation->email)->send(new ReservationDeclined($reservation));
 
         return redirect()->back()->with('success', 'Reservation has been declined.');
