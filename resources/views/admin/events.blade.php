7b97050f090589fdb41c0f3e5ec7453bc1c77ffd
kholoud001 <kholoud.sanak@gmail.com>
2024-03-10 12:29:00 +0000

    update admin event list

--- resources/views/admin/events.blade.php
+++ resources/views/admin/events.blade.php
@@ -34,7 +34,7 @@
                         <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Events</p>
                     </th>
                     <th class="border-b border-blue-gray-50 py-3 px-6 text-left">
-                        <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Deadline</p>
+                        <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Organizer</p>
                     </th>
                     <th class="border-b border-blue-gray-50 py-3 px-6 text-left">
                         <p class="block antialiased font-sans text-[11px] font-medium uppercase text-blue-gray-400">Status</p>
@@ -46,12 +46,13 @@
                 </thead>
                 <tbody>
                 @foreach($events as $event)
+
                     <tr>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
                             <p class="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold">{{ $event->titre }}</p>
                         </td>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
-                            <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">{{ $event->deadline }}</p>
+                            <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">{{ $event->organizer->name }}</p>
                         </td>
                         <td class="py-3 px-5 border-b border-blue-gray-50">
                             <p class="block antialiased font-sans text-xs font-medium text-blue-gray-600">
